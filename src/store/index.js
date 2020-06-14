0bafc308256d89fd2d8ae716a25d32cd2d73427a
Wellington Dias <wads90@gmail.com>
2020-06-14 10:45:01 -0300

    fix REDUX_DEVTOOLS_EXTENSION

--- src/store/index.js
+++ src/store/index.js
@@ -16,7 +16,9 @@ const store = createStore(
   persistedReducer,
   compose(
     applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ...(window.__REDUX_DEVTOOLS_EXTENSION__
+      ? [window.__REDUX_DEVTOOLS_EXTENSION__()]
+      : [])
   )
 );
 
